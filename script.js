e5de242fb4cf906888ebff3357e3cbc3630b861a
niteshgoswami72812-create <niteshgoswami72812@gmail.com>
2026-04-12 14:54:21 +0530

    Nitesh

--- script.js
+++ script.js
@@ -10,4 +10,13 @@ function typeWriter() {
         setTimeout(typeWriter, typingSpeed);
     }
 }
-window.onload = typeWriter;
+
+window.onload = () => {
+    
+    setTimeout(() => {
+        document.getElementById('welcome-screen').classList.add('fade-out');
+        
+        
+        setTimeout(typeWriter, 500); 
+    }, 2000);
+};
